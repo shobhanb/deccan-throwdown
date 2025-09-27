d924d75198dd8a7b8b1345612402672adbe3c6d9
Shobhan Bhatt <shobhanb@gmail.com>
2025-09-28 01:32:11 +0530

    Changed Advanced to Open

--- backend/app/even_scoring_tables/constants.py
+++ backend/app/even_scoring_tables/constants.py
@@ -2,7 +2,7 @@ EVEN_SCORING_TABLES_ASSIGNMENT = {
     "dtteams2025": {
         "Beginner": "20 to 18 athletes",
         "Intermediate": "15 to 14 athletes",
-        "Advanced": "10 or fewer athletes",
+        "Open": "10 or fewer athletes",
     },
     "dtpairs2025": {
         "FF-Beginners": "10 or fewer athletes",
